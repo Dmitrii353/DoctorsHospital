d7cd3d51328bec6a977a48d435ff0ebd0a547523
Dmitriy Lyuft <dimarik0917@gmail.com>
2024-12-30 18:34:04 +0500

    Initial Commit

---
+++ DoctorsHospital/DoctorsHospitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoctorsHospitalApp.swift
+//  DoctorsHospital
+//
+//  Created by Дима Люфт on 30.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DoctorsHospitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
